ef9b928781b8e1ae2950cd874a24ec9339ca719f
Olivier <ovslash@hotmail.fr>
2023-09-07 10:31:46 +0200

    ajout set de produit au lancement de l appli

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,10 @@
 INSERT  INTO type_produit (id,libelle) values (1,'pizza');
 INSERT  INTO type_produit (id,libelle) values (2,'boisson');
+
+INSERT INTO produit ( nom, description, prix, url_image, type_produit_id) VALUES('Tropicale Pizzas', 'Sauce barbecue, mozzarella, pepperoni, ananas.', 15, 'https://www.dominos.fr/ManagedAssets/FR/product/PTRO/FR_PTRO_fr_hero_12171.png?v524299518',1);
+INSERT INTO produit ( nom, description, prix, url_image, type_produit_id) VALUES('Cannibale Pizzas', 'Sauce barbecue, mozzarella, poulet rôti, merguez, haché au bœuf assaisonné.', 14, 'https://www.dominos.fr/ManagedAssets/FR/product/PCAN/FR_PCAN_fr_hero_12192.png?v1242436109',1);
+
+INSERT INTO produit ( nom, description, prix, url_image, type_produit_id) VALUES('4 Fromages Pizzas', 'Sauce tomate, mozzarella, chèvre, emmental, Fourme d’Ambert AOP.', 15, 'https://www.dominos.fr/ManagedAssets/FR/product/P4FR/FR_P4FR_fr_hero_12192.png?v-280271063',1);
+INSERT INTO produit ( nom, description, prix, url_image, type_produit_id) VALUES('Savoyarde Pizzas', 'Crème fraîche légère française, mozzarella, lardons fumés, pommes de terre françaises sautées, Reblochon de Savoie AOP, origan.', 14, 'https://www.dominos.fr/ManagedAssets/FR/product/PSVY/FR_PSVY_fr_hero_12192.png?v-447122871',1);
+
+INSERT INTO produit ( nom, description, prix, url_image, type_produit_id) VALUES('Badoit - 50cl', 'Finement Pétillante, c est l eau de Badoit incontournable, intemporelle. Ses fines bulles empreintes de légèreté et son goût unique et délicat accompagnent tous vos repas au quotidien.', 3 , 'https://www.dominos.fr/ManagedAssets/FR/product/DBAD0500/FR_DBAD0500_fr_hero_3945.png?v-1292294104',2);
